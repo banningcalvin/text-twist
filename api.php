808306d17d7b9ec78b4b5ff8117a9fb5e425f85e
cb <cbanning@udel.edu>
2019-09-18 18:50:01 -0400

    changed dict db

--- api.php
+++ api.php
@@ -1,14 +1,14 @@
 <?php
 
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
-    $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
+    $dbhandle = new PDO("dict:dict.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
  
     //this is a sample query which gets some data, the order by part shuffles the results
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
     // ordering by rank, etc.
-    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
+    $query = "SELECT * FROM words order by random() limit 0, 10";
     
     //this next line could actually be used to provide user_given input to the query to 
     //avoid SQL injection attacks
