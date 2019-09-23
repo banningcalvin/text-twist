1376cda36c0bf925c7dd5c9dd6fad5089b5b4355
cb <cbanning@udel.edu>
2019-09-23 17:32:59 -0400

    updates

--- api.php
+++ api.php
@@ -1,29 +1,61 @@
 <?php
 
-    //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
-    $dbhandle = new PDO("sqlite:words.sqlite") or die("Failed to open DB");
+
+
+    // Get all subracks and submit
+	function generate_rack($length){
+  		$tileBag = "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
+  		$rack_letters = substr(str_shuffle($tileBag), 0, $length);
+  
+  		$temp = str_split($rack_letters);
+  		sort($temp);
+  		return implode($temp);
+    };
+    
+	$rack = generate_rack(7);
+	$racks = [];
+	for($i = 0; $i < pow(2, strlen($rack)); $i++){
+		$answer = "";
+		for($j = 0; $j < strlen($rack); $j++){
+			if (($i >> $j) % 2) {
+			  $answer .= $rack[$j];
+			}
+		}
+		if (strlen($answer) > 1){
+			$racks[] = $answer;	
+		}
+	}
+	$racks = array_unique($racks);
+
+	$response = array('letters' => $rack, 'words' => array());
+
+
+
+
+
+
+    $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
  
-    //returns random word without a hyphen or dash between 5 and 7 letters
-    $query = "SELECT * FROM WORDS WHERE WORD NOT LIKE '% %' AND WORD NOT LIKE '%-%' AND LENGTH(WORD) > 4 AND LENGTH(WORD) < 8 ORDER BY RANDOM() LIMIT 1";
-    
-    //this next line could actually be used to provide user_given input to the query to 
-    //avoid SQL injection attacks
-    $statement = $dbhandle->prepare($query);
-    $statement->execute();
-    
-    //The results of the query are typically many rows of data
-    //there are several ways of getting the data out, iterating row by row,
-    //I chose to get associative arrays inside of a big array
-    //this will naturally create a pleasant array of JSON data when I echo in a couple lines
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    
-    //this part is perhaps overkill but I wanted to set the HTTP headers and status code
-    //making to this line means everything was great with this request
-    header('HTTP/1.1 200 OK');
-    //this lets the browser know to expect json
-    header('Content-Type: application/json');
-    //this creates json and gives it back to the browser
-    echo json_encode($results);
+    foreach ($racks as $subrack) {
+        $query = "SELECT words FROM racks WHERE rack = ?";
+        
 
+        $statement = $dbhandle->prepare($query);
+        $statement->bindParam(1, $subrack, PDO::PARAM_STR);
+        $statement->execute();
+        
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        foreach ($results as $row) {
+            $response['words'] = array_merge(
+                $response['words'],
+                explode('@@', $row['words'])
+            );
+        }
+    }
+    
+    header('HTTP/1.1 200 OK');
+    header('Content-Type: application/json');
+    echo json_encode($response);
 ?>
